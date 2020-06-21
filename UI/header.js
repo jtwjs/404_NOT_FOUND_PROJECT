d93a7a63cd283fe2801825e8386546da5714a0f5
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2020-06-21 14:51:05 +0900

    update: footer
    1. add: wraper drop menu
    2. update: cs_detail-content change of location

--- UI/header.js
+++ UI/header.js
@@ -11,6 +11,17 @@ var dropBtn = document.getElementById('nav__category');
     });
 
     document.getElementById('category_btn').addEventListener('mouseover',function(){
+        document.getElementById('sub_category--first').classList.remove('hide');
+        document.getElementById('sub_category--second').classList.add('hide');
+        document.getElementById('sub_category--third').classList.add('hide');
+        document.getElementById('sub_category--fourth').classList.add('hide');
+        document.getElementById('sub_category--fifth').classList.add('hide');
+        document.getElementById('sub_category--sixth').classList.add('hide');
+        document.getElementById('sub_category--seventh').classList.add('hide');
+        document.getElementById('sub_category--eighth').classList.add('hide');
+        document.getElementById('sub_category--ninth').classList.add('hide');
+        document.getElementById('sub_category--tenth').classList.add('hide');
+
         document.getElementById('main_category--first').classList.add('title-on');
         document.getElementById('main_category--second').classList.remove('title-on');
         document.getElementById('main_category--third').classList.remove('title-on');
